ab9da07c1e4dc07de6370e09d7eefa2e2f48408f
Ekke <Ekke@EKKE-PC>
2018-12-06 21:19:26 +0200

    Started hooking up strategies and data providers to a module

--- Modules/DingWatGeldMaak.FOREX/ForexModule.cs
+++ Modules/DingWatGeldMaak.FOREX/ForexModule.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DingWatGeldMaak.Core.Providers;
+using DingWatGeldMaak.FOREX.Providers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,18 +10,23 @@ namespace DingWatGeldMaak.FOREX
 {
   public class ForexModule : Core.Modules.Module
   {
-    //public DataProvider
+    protected Dictionary<string, PriceDataProvider> _dataProviders = null;
 
     public ForexModule() : base()
     {
       _Id = Guid.Parse("40C11D6C-4FC4-4D05-ADD5-96EE00D4E42A");
 
       _Name = "ForexModule";
+
+      _dataProviders = new Dictionary<string, PriceDataProvider>();
     }
 
     public override void Start()
     {
       base.Start();
+
+      //Load config
+
     }
 
     public override void Stop()
