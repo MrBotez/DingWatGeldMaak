2ef824efda73d710e1728c89bed1b7c4a05ef84c
MrBotez <riaanbotez@gmail.com>
2018-12-07 20:13:55 +0200

    Introduced history data provier

---
+++ Modules/DingWatGeldMaak.FOREX/Providers/HistoryDataProvider.cs
@@ -0,0 +1,73 @@
+﻿using DingWatGeldMaak.Core.Providers;
+using DingWatGeldMaak.FOREX.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DingWatGeldMaak.FOREX.Providers
+{
+  public class HistoryDataProvider : TimeIntervalDataProvider<OHLC>
+  {
+    protected string fileName = "";
+    protected List<OHLC> historyData = null;
+    protected int currentIndex = 0;
+
+    public HistoryDataProvider(string fileName) : base()
+    {
+      this.fileName = fileName;
+      historyData = new List<OHLC>();
+    }
+
+    public override void Dispose()
+    {
+      historyData.Clear();
+
+      base.Dispose();
+    }
+
+    public override void Start()
+    {
+      currentIndex = 0;
+      historyData.Clear();
+
+      if (File.Exists(fileName))
+      {
+        foreach (var line in File.ReadAllLines(fileName))
+        {
+          var flds = line.Split(',');
+
+          var dateComponents = flds[0].Split('.').Select(i => Convert.ToInt32(i)).ToList();
+          dateComponents.AddRange(flds[1].Split(':').Select(i => Convert.ToInt32(i)));
+
+          historyData.Add(new OHLC()
+            .SetTime(new System.DateTime(dateComponents[0], dateComponents[1], dateComponents[2], dateComponents[3], dateComponents[4], 0))
+            .SetOpen(Convert.ToDouble(flds[2]))
+            .SetHigh(Convert.ToDouble(flds[3]))
+            .SetLow(Convert.ToDouble(flds[4]))
+            .SetClose(Convert.ToDouble(flds[5]))
+            .SetVolume(Convert.ToInt32(flds[6]))
+          );
+        }
+      }
+
+      base.Start();
+    }
+
+    public override void Get()
+    {
+      if (currentIndex < historyData.Count)
+      {
+        var data = new List<OHLC>() { historyData[currentIndex] };
+
+        RaiseDataAvailable(data);
+
+        currentIndex++;
+      }
+      else
+      {
+        RaiseError("No more data", null);
+      }
+    }
+  }
+}
